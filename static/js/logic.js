77a5b29b5b2b7658cc458be1d613118a82f76456
Elona <exhikola@yahoo.com>
2024-01-29 21:15:39 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,1 +1,122 @@
+// API key
+const API_KEY = "pk.eyJ1IjoidHN0cmljazI5IiwiYSI6ImNsNWEzeWYxajAwZzQzam1xczZtNGcyNTgifQ.hu7Hr-OAgx_YeG89WyaTdA";
+
+// Store geoJSON
+const link = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+
+// Perform a GET request to the query URL
+d3.json(link).then((data) => {
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
+});
+
+function createMap(earthquakes, tectonicPlates) {
+    // Assign different mapbox styles
+    const satellite = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 20,
+        id: 'mapbox.satellite',
+        accessToken: API_KEY
+    });
+
+    const grayscale = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 20,
+        id: 'mapbox.light',
+        accessToken: API_KEY
+    });
+
+    const outdoors = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 20,
+        id: 'mapbox.outdoors',
+        accessToken: API_KEY
+    });
+
+    const baseMap = {
+        'Satellite': satellite,
+        'Grayscale': grayscale,
+        'Outdoors': outdoors
+    };
+
+    const overlayMap = {
+        Earthquakes: earthquakes,
+        'Tectonic Plates': tectonicPlates
+    };
+
+    const myMap = L.map('map', {
+        center: [36.7126875, -120.476189],
+        zoom: 4,
+        layers: [outdoors, earthquakes, tectonicPlates]
+    });
+
+    L.control.layers(baseMap, overlayMap, {
+        collapsed: false
+    }).addTo(myMap);
+
+    // Function to assign colors for legend and markers
+    function getColor(d) {
+        return d > 5 ? '#f06b6b' :
+            d > 4 ? '#f0936b' :
+            d > 3 ? '#f3ba4e' :
+            d > 2 ? '#f3db4c' :
+            d > 1 ? '#e1f34c' :
+                    '#b7f34d';
+    }
+
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function(myMap) {
+        const div = L.DomUtil.create('div', 'info legend');
+        const magnitudes = [0, 1, 2, 3, 4, 5];
+        
+        for (let i = 0; i < magnitudes.length; i++) {
+            div.innerHTML +=
+            '<i style="background:' + getColor(magnitudes[i] + 1) + '"></i>' + magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+    legend.addTo(myMap);
+}
+
+function createFeatures(eqdata) {
+    function onEachFeature(feature, layer) {
+        layer.bindPopup('<h4>Place: ' + feature.properties.place + '</h4><h4>Date: ' + new Date(feature.properties.time) + '</h4><h4>Magnitude: ' + feature.properties.mag + '</h4><h4>USGS Event Page: <a href=' + feature.properties.url + " target='_blank'>Click here</a></h4>", {maxWidth: 400});
+    }
+
+    const layerToMap = L.geoJSON(eqdata, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function(feature, latlng) {
+            let radius = feature.properties.mag * 4.5;
+            let fillcolor;
+
+            if (feature.properties.mag > 5) {
+                fillcolor = '#f06b6b';
+            }
+            else if (feature.properties.mag >= 4) {
+                fillcolor = '#f0936b';
+            }
+            else if (feature.properties.mag >= 3) {
+                fillcolor = '#f3ba4e';
+            }
+            else if (feature.properties.mag >= 2) {
+                fillcolor = '#f3db4c';
+            }
+            else if (feature.properties.mag >= 1) {
+                fillcolor = '#e1f34c';
+            }
+            else  fillcolor = '#b7f34d';
+
+            return L.circleMarker(latlng, {
+                radius: radius,
+                color: 'black',
+                fillColor: fillcolor,
+                fillOpacity: 1,
+                weight: 1
+            });
+        }
+    });
+
+    // Load tectonic plate boundaries GeoJSON
+    d3.json('path/to/tectonic_plate_boundaries.geojson').then((tectonicPlatesData) => {
+        createMap(layerToMap, tectonicPlatesData.features);
+    });
+}
 
